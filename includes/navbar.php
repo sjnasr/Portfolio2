64869e4d3678643be9507a88b5d0348267edbdbd
sjnasr <sjnasr18@yahoo.com>
2016-03-29 19:46:46 -0700

    Changed everything from php to html

---
+++ includes/navbar.php
@@ -0,0 +1,36 @@
+<header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner" style="background-color: #6A0888;">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="images/logo copy.png" alt="logo"></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about-us.html">About Us</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="career.html">Career</a></li>
+                        <li><a href="blog-item.html">Blog Single</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li><a href="404.html">404</a></li>
+                        <li><a href="registration.html">Registration</a></li>
+                        <li class="divider"></li>
+                        <li><a href="privacy.html">Privacy Policy</a></li>
+                        <li><a href="terms.html">Terms of Use</a></li>
+                    </ul>
+                </li>
+                <li><a href="blog.html">Blog</a></li> 
+                <li><a href="contact-us.html">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
